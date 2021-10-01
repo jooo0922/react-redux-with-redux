84a66bb91d4af9660411b13e49f6346bf003dca2
jooo0922 <2014102458@khu.ac.kr>
2021-10-02 05:56:33 +0900

    docs: Copy without-redux project files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import AddNumberRoot from "./components/AddNumberRoot";
+import DisplayNumberRoot from "./components/DisplayNumberRoot";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    number: 0,
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Root</h1>
+        <AddNumberRoot
+          onClick={function (size) {
+            this.setState({ number: this.state.number + size });
+          }.bind(this)}
+        ></AddNumberRoot>
+        <DisplayNumberRoot number={this.state.number}></DisplayNumberRoot>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+/**
+ * jsx의 input 태그의 경우 readOnly라는 attribute를 할당하면
+ * input 사용자가 태그 안에 값을 수정하지 못하도록 함
+ */
